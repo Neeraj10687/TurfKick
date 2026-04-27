b4a8f83584d505f4c0021f0d5040c69e61dae677
Neeraj <109914099+Neeraj10687@users.noreply.github.com>
2026-04-27 22:37:25 +0530

    final

--- admin_update.sql
+++ admin_update.sql
@@ -4,4 +4,4 @@ ALTER TABLE users MODIFY COLUMN role ENUM('user', 'owner', 'admin') DEFAULT 'use
 -- Add a sample admin if not exists
 INSERT INTO users (name, email, password, role) 
 VALUES ('System Admin', 'admin@turfkick.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin')
-ON DUPLICATE KEY UPDATE role='admin';
+ON DUPLICATE KEY UPDATE role='admin', password=VALUES(password);
